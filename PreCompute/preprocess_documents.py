66fb14feba6a00533ed9a855b3b97332c46506ec
sukriti10.tiwari@gmail.com <sukriti10>
2019-10-24 14:49:07 -0400

    Adding python file for preprocess, and dumped csv

---
+++ PreCompute/preprocess_documents.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd 
+import numpy as np 
+import zipfile
+import os
+import re, nltk
+from sklearn.feature_extraction.text import CountVectorizer        
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+import html
+import copy
+import inflect
+from nltk.corpus import stopwords
+
+import re, string, unicodedata
+import nltk
+
+def create_dataframe(zip_path='../data/bbc.zip'):
+    datadir = './bbc'
+    zip = zipfile.ZipFile(zip_path)
+    zip.extractall()
+
+    dirs = os.listdir( datadir )
+    data = []
+    for each in dirs:
+        if each=='README.TXT':
+            continue
+        sub_dir = os.listdir(datadir+'/'+each)
+        for every in sub_dir:
+            with open(datadir+'/'+each+'/'+every, encoding='utf-8',errors='ignore') as f:
+                text = f.read()
+            data.append([text,each])
+    return pd.DataFrame(data,columns=['text','label'])
+
+
+# In[147]:
+
+
+def fixup(x):
+    re1 = re.compile(r'  +')
+    x = x.replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace(
+        'nbsp;', ' ').replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace(
+        '<br />', "\n").replace('\\"', '"').replace('<unk>','u_n').replace(' @.@ ','.').replace(
+        ' @-@ ','-').replace('\\', ' \\ ').replace('>', '').replace('-', '')
+    return re1.sub(' ', html.unescape(x))
+
+stemmer = PorterStemmer()
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+stop_words = set(stopwords.words('english')) 
+def tokenize(text):
+    # remove non letters
+    text = re.sub(" \d+", " ", text)
+    # tokenize
+    tokens = nltk.word_tokenize(text)
+    # Remove stop words 
+    tokens = [w for w in tokens if not w in stop_words]
+    tokens = [w for w in tokens if  len(w)>=2]
+    tokens = [fixup(x) for x in tokens]
+    # stem
+    stems = stem_tokens(tokens, stemmer)
+    return tokens
+
+contraction_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+def _get_contractions(contraction_dict):
+
+    contraction_re = re.compile('(%s)' % '|'.join(contraction_dict.keys()))
+    return contraction_dict, contraction_re
+
+contractions, contractions_re = _get_contractions(contraction_dict)
+
+def replace_contractions(text):
+    def replace(match):
+        return contractions[match.group(0)]
+    return contractions_re.sub(replace, text)
+
+
+def remove_non_ascii(words):
+    """Remove non-ASCII characters from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    return new_words
+
+def to_lowercase(words):
+    """Convert all characters to lowercase from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+def remove_punctuation(words):
+    """Remove punctuation from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+def remove_stopwords(words):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+def stem_words(words):
+    """Stem words in list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+def normalize(words):
+    words = [fixup(x) for x in words]
+    words = to_lowercase(words)
+    words = remove_non_ascii(words)
+    words = [replace_contractions(x) for x in words]
+    words = remove_punctuation(words)
+    words = replace_numbers(words)
+    words = remove_stopwords(words)
+    words = [tokenize(x) for x in words]
+    return words
+
+
+dat = create_dataframe()
+dat.text = normalize(dat.text)
+dat.to_csv('../data/cleaned_documents.csv')
