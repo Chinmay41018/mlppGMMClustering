a7f4b24e8c94212519710df33fd9c7d3eea7ca93
sukriti10.tiwari@gmail.com <sukriti10>
2019-11-19 19:26:28 -0500

    Criticism module.

---
+++ criticism_module.py
@@ -0,0 +1,61 @@
+import torch
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import TSNE
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LogisticRegression
+
+
+color_map = {0: 'red',
+             1: 'green',
+             2: 'blue',
+             3: 'yellow',
+             4: 'pink'}
+en = {'business': 0,
+      'entertainment': 1,
+      'politics': 2,
+      'sport': 3,
+      'tech': 4}
+
+
+def tsne_plot(emb, labels, color_map=color_map, n_comp=2):
+    low_emb = TSNE(n_components=2).fit_transform(emb)
+    fig = plt.figure()
+    plt.scatter(
+        low_emb[:, 0], low_emb[:, 1], c=[color_map[y] for y in labels])
+    plt.show()
+    fig.savefig('tsne_plot')
+
+
+def criticise(path_to_d):
+    dat = torch.load(path_to_d, map_location=torch.device('cpu'))
+    labels = pd.read_csv(
+        './data/cleaned_documents.csv',
+        header=0,
+        names=['text', 'label', 'embedding']).labels
+    encoded_label = [en[y] for y in labels]
+    model = LogisticRegression()
+    cv_score = cross_validate(model, dat.tolist(), encoded_label, cv=3)
+    print("Classification performance : \n", np.mean(cv_score))
+    return np.array([x.data.numpy() for x in dat]), labels
+
+
+def criticise_gold():
+    def lam_doc2vec(x): return [float(y) for y in (x.strip("[]").split(", "))]
+    doc2vec_dat = pd.read_csv(
+                        './data/embedded_doc2vec_bbc.csv',
+                        header=0,
+                        names=['text', 'label', 'embedding'],
+                        converters={"embedding": lam_doc2vec})
+    doc2vec_emb = doc2vec_dat.embedding.values
+    labels = doc2vec_dat.label.values
+    encoded_label = [en[y] for y in labels]
+    model = LogisticRegression()
+    cv_score = cross_validate(model, doc2vec_emb.tolist(), encoded_label, cv=3)
+    print(
+        "Doc2Vec classification performance : \n",
+        np.mean(cv_score['test_score']))
+    return doc2vec_emb.tolist(), encoded_label
