efc5adc888170e00c43cf2d1934567425d103325
Chinmay41018 <chinmayjoshi75@gmail.com>
2019-11-19 20:17:30 -0500

    Almost do done

--- criticism_module.py
+++ criticism_module.py
@@ -27,7 +27,6 @@ def tsne_plot(emb, labels, color_map=color_map, n_comp=2):
     plt.scatter(
         low_emb[:, 0], low_emb[:, 1], c=[color_map[y] for y in labels])
     plt.show()
-    fig.savefig('tsne_plot')
 
 
 def criticise(path_to_d):
@@ -35,12 +34,12 @@ def criticise(path_to_d):
     labels = pd.read_csv(
         './data/cleaned_documents.csv',
         header=0,
-        names=['text', 'label', 'embedding']).labels
+        names=['text', 'embedding', 'label']).label
     encoded_label = [en[y] for y in labels]
     model = LogisticRegression()
-    cv_score = cross_validate(model, dat.tolist(), encoded_label, cv=3)
-    print("Classification performance : \n", np.mean(cv_score))
-    return np.array([x.data.numpy() for x in dat]), labels
+    cv_score = cross_validate(model, np.array([x.data.numpy() for x in dat]), encoded_label, cv=3)
+    print("Classification performance : ", np.mean(cv_score['test_score']))
+    return np.array([x.data.numpy() for x in dat]), encoded_label
 
 
 def criticise_gold():
@@ -56,6 +55,6 @@ def criticise_gold():
     model = LogisticRegression()
     cv_score = cross_validate(model, doc2vec_emb.tolist(), encoded_label, cv=3)
     print(
-        "Doc2Vec classification performance : \n",
+        "Doc2Vec classification performance : ",
         np.mean(cv_score['test_score']))
     return doc2vec_emb.tolist(), encoded_label
